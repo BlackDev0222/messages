b727f47cbe15ee5c6c16c4a30ae0d39391807fd5
BlackDev0222 <BlackDev0222@gmail.com>
2016-08-02 15:29:22 -0600

    update models\post.js

---
+++ models/post.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var moment = require('../build/moment.min.js'); // Time formatter
+
+var PostSchema = new Schema({
+  name: String,
+  title: String,
+  message: String,
+  createdAt: {type:String, default: function() {
+    return moment().format('MMMM Do YYYY, h:mm a');  }
+  }
+});
+
+mongoose.model('post', PostSchema);
